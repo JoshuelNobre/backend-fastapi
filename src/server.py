9650e3c86262d066050351e3223f8af6fc18fff1
Joshuel <nobrej1999@gmail.com>
2021-12-21 22:33:15 -0300

    migrations

--- src/server.py
+++ src/server.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
+from typing import List
 from sqlalchemy.orm import session
 from sqlalchemy.orm.session import Session
-from src.schemas.schemas import Produto
+from src.infra.sqlalchemy.repositorios.usuarios import RepositorioUsuario
+from src.schemas.schemas import Produto, Usuario, ProdutoSimples
 from src.infra.sqlalchemy.repositorios.produtos import RepositorioProduto
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
 
@@ -9,11 +11,17 @@ criar_bd()
 
 app = FastAPI()
 
-@app.post('/produtos', response_model=Produto)
+@app.post('/produtos', response_model=ProdutoSimples, status_code=status.HTTP_201_CREATED)
 def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
-@app.get('/produtos')
-def listar_produtos():
-    return {'msg':'listando produtos'}
+@app.get('/produtos', response_model=List[Produto], status_code=status.HTTP_200_OK)
+def listar_produtos(db: Session = Depends(get_db)):
+    produtos = RepositorioProduto(db).listar()
+    return produtos
+
+@app.post('/usuario', status_code=status.HTTP_201_CREATED)
+def criar_usuario(usuario: Usuario, db:Session = Depends(get_db)):
+    usuario_criado = RepositorioUsuario(db).criar(usuario)
+    return usuario_criado
