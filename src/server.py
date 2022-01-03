363ae0046e93f1ec6ea4174cb3eb9692210a208a
Joshuel <nobrej1999@gmail.com>
2022-01-03 00:14:35 -0300

    edit

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from src.routers import rotas_pedidos, rotas_produtos, rotas_usuarios
 
+# RUN
 app = FastAPI()
 
 # CORS
